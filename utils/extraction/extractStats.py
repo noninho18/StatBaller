c13400ce1cdb9ace5087fb2cfe387023e8dff2e6
noninho18 <elion.hashani@telecomnancy.net>
2024-10-16 13:05:21 +0200

    [+] Rearranging the functions

--- utils/extraction/extractStats.py
+++ utils/extraction/extractStats.py
@@ -7,6 +7,34 @@ def scrape_data(url, columns):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
 
+    table_soup = BeautifulSoup(response.text, 'html.parser')
+    rows = table_soup.find_all('tr')
+
+    data = []
+
+    if rows:
+        result = []
+        for row in rows[2:]:
+            cells = row.find_all(['th', 'td'])
+
+            if cells[1].text.strip() == "Player":
+                continue
+
+            row_data = [cell.text.strip() for cell in cells]
+            data.append(row_data)
+
+        df = pd.DataFrame(data, columns=columns)
+        df['Nation'] = df['Nation'].apply(lambda x: x[-3:].strip())
+        df['Comp'] = df['Comp'].apply(lambda x: " ".join(x.split()[1:]))
+        df.drop(columns=['Rk','Matches'], inplace=True)
+        return df
+    else:
+        return "No rows found in the table."
+
+def scrape_commented_data(url,columns): 
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
     comments = soup.find_all(string=lambda text: isinstance(text, Comment))
     
     table_html = None
@@ -26,15 +54,17 @@ def scrape_data(url, columns):
             for row in rows[2:]:
                 cells = row.find_all(['th', 'td'])
 
-                if cells[1].text.strip() == "Joueur":
+                if cells[1].text.strip() == "Player":
                     continue
 
                 row_data = [cell.text.strip() for cell in cells]
                 data.append(row_data)
 
             df = pd.DataFrame(data, columns=columns)
+            print(df)
             df['Nation'] = df['Nation'].apply(lambda x: x[-3:].strip())
-            df.drop(columns=['Clt','Matchs'], inplace=True)
+            df['Squad'] = df['Squad'].apply(lambda x: " ".join(x.split()[1:]))
+            df.drop(columns=['Rk','Matches'], inplace=True)
             return df
         else:
             return "No rows found in the table."
--- utils/extraction/mainExtraction.py
+++ utils/extraction/mainExtraction.py
@@ -1,35 +1,36 @@
-from utils.extraction.extractStats import scrape_data
+from utils.extraction.extractStats import scrape_data, scrape_commented_data
 from utils.csv.to_csv import into_csv, combine_csv
 
 def data_to_csv(folder, leagues, columns, filename):
     league_data = []
     for league_name, url in leagues.items():
         print(f"Scraping {league_name}...")
-        data = scrape_data(url, columns)
+        if league_name == 'Champions_League':
+            columns.remove('Comp')
+            data = scrape_commented_data(url, columns)
+        else: 
+            data = scrape_data(url, columns)
         print(f"{league_name} scraped successfully with {len(data)} rows.")
         league_data.append(data)
         print(data)
         into_csv(data, f"{league_name}_"+filename+"_stats.csv", folder)
 
-    combine_csv(league_data, filename+'_league_stats.csv', folder)
+    #combine_csv(league_data, filename+'_league_stats.csv', folder)
 
 def basic_stats_scrape():
     folder = "Data/basic_stats"
 
     leagues = {
-        'Ligue_1': 'https://fbref.com/fr/comps/13/stats/Statistiques-Ligue-1',
-        'Premier_League': 'https://fbref.com/fr/comps/9/stats/Statistiques-Premier-League',
-        'La_Liga': 'https://fbref.com/fr/comps/12/stats/Statistiques-La-Liga',
-        'Serie_A': 'https://fbref.com/fr/comps/11/stats/Statistiques-Serie-A',
-        'Bundesliga': 'https://fbref.com/fr/comps/20/stats/Statistiques-Bundesliga'
+        'Top_5_Leagues' : 'https://fbref.com/en/comps/Big5/stats/players/Big-5-European-Leagues-Stats',
+        'Champions_League' : 'https://fbref.com/en/comps/8/stats/Champions-League-Stats'
     }
     
     columns = [
-        'Clt', 'Joueur', 'Nation', 'Pos', 'Équipe', 'Âge', 'Naissance', 'MJ', 
-        'Titulaire', 'Min', '90', 'Buts', 'PD', 'B+PD', 'B-PénM', 'PénM', 
-        'PénT', 'CJ', 'CR', 'xG', 'npxG', 'xAG', 'npxG+xAG', 'PrgC', 
-        'PrgP', 'PrgR', 'Buts/90', 'PD/90', 'B+PD/90', 'B-PénM/90', 
-        'B+PD-PénM/90', 'xG/90', 'xAG/90', 'xG+xAG/90', 'npxG/90', 'npxG+xAG/90', 'Matchs'
+        'Rk', 'Player', 'Nation', 'Pos', 'Squad', 'Comp', 'Age', 'Born', 'MatchesPlayed', 'Starts', 
+        'Min', '90s', 'Goals', 'Assists', 'G+A', 'G-PK', 'PK', 'PKattempted', 'CardY', 'CardR', 
+        'xG', 'npxG', 'xAG', 'npxG+xAG', 'PrgCarries', 'PrgPasses', 'PrgReceived', 
+        'G/90', 'A/90', 'G+A/90', 'G-PK/90', 'G+A-PK/90', 'xG/90', 'xAG/90', 
+        'xG+xAG/90', 'npxG/90', 'npxG+xAG/90', 'Matches'
     ]
 
     data_to_csv(folder, leagues, columns,"Basic")
@@ -38,17 +39,14 @@ def GK_stats_scrape():
     folder = "Data/gk_stats"
 
     leagues = {
-        'Ligue_1': 'https://fbref.com/fr/comps/13/keepers/Statistiques-Ligue-1',
-        'Premier_League': 'https://fbref.com/fr/comps/9/keepers/Statistiques-Premier-League',
-        'La_Liga': 'https://fbref.com/fr/comps/12/keepers/Statistiques-La-Liga',
-        'Serie_A': 'https://fbref.com/fr/comps/11/keepers/Statistiques-Serie-A',
-        'Bundesliga': 'https://fbref.com/fr/comps/20/keepers/Statistiques-Bundesliga'
+        'Top_5_Leagues' : 'https://fbref.com/en/comps/Big5/keepers/players/Big-5-European-Leagues-Stats',
+        'Champions_League' : 'https://fbref.com/en/comps/8/keepers/Champions-League-Stats'
     }
 
     columns = [
-        'Clt', 'Joueur', 'Nation', 'Pos', 'Équipe', 'Âge', 'Naissance', 'MJ', 
-        'Titulaire', 'Min', '90', 'ButEncaisse', 'ButEncaisse90', 'TCC', 'Arrêts', 'Arrêts%', 
-        'V', 'N', 'D', 'CleanSheet', 'CleanSheet%', 'PenTire', 'PenConcede', 'PenArrete', 'PenManques', 'Arrêts%', 'Matchs'
+        'Rk', 'Player', 'Nation', 'Pos', 'Squad', 'Comp', 'Age', 'Born', 'MatchesPlayed', 'Starts', 
+        'Min', '90s', 'GoalsConceded', 'GoalsConceded90', 'SoTAgainst', 'Saves', 'Saves%', 
+        'W', 'D', 'L', 'CleanSheet', 'CleanSheet%', 'PKatt', 'PKConcede', 'PKSaved', 'PKMissed', 'PKSaved%', 'Matches'
     ]
     
     data_to_csv(folder, leagues, columns,"GK")
@@ -57,20 +55,15 @@ def GK_adv_stats_scrape():
     folder = "Data/gk_advanced_stats"
 
     leagues = {
-        'Ligue_1': 'https://fbref.com/fr/comps/13/keepersadv/Statistiques-Ligue-1',
-        'Premier_League': 'https://fbref.com/fr/comps/9/keepersadv/Statistiques-Premier-League',
-        'La_Liga': 'https://fbref.com/fr/comps/12/keepersadv/Statistiques-La-Liga',
-        'Serie_A': 'https://fbref.com/fr/comps/11/keepersadv/Statistiques-Serie-A',
-        'Bundesliga': 'https://fbref.com/fr/comps/20/keepersadv/Statistiques-Bundesliga'
+        'Top_5_Leagues' : 'https://fbref.com/en/comps/Big5/keepersadv/players/Big-5-European-Leagues-Stats',
+        'Champions_League' : 'https://fbref.com/en/comps/8/keepersadv/Champions-League-Stats'
     }
 
     columns = [
-        'Clt', 'Joueur', 'Nation', 'Pos', 'Équipe', 'Âge', 'Naissance', '90', 
-        'Buts', 'Attendu', 'Degagement', 'Passes', 'Degagements6m', 
-        'Centres', 'Libéro', 'ButEncaisse', 'PenConcede', 'ButCF', 'ButCorner', 'CSC', 'PostShotxG', 'PostShotxG/ShotonTarget', 
-        'PostShotxG+/-', 'PostShotxG/90', 'GoalKickCompleted', 'GoalKickAttempted', 'GoalKickCompleted%', 'PassAttempted', 
+        'Rk', 'Player', 'Nation', 'Pos', 'Squad', 'Comp', 'Age', 'Born', '90s', 'GoalsConceded', 'PKConceded', 'GoalsFK', 'GoalsCorner', 'CSC', 'PSxG', 'PSxG/SoT', 
+        'PSxG+/-', 'PSxG/90', 'LaunchCompleted', 'LaunchAttempted', 'LaunchCompleted%', 'PassAttempted', 
         'ThrowAttempted', "% of throws", 'PassAvgLen', 'GoalKicksAtt', '% of GoalKicks(+35yards)', 'GoalKicksAvgLen', 
-        'OppCrosses', 'CrossesStopped', 'Stp%', 'NbrActionLibero', 'NbrActionLibero/90', 'DistMoy', 'Matchs'
+        'OppCrosses', 'CrossesStopped', 'Stp%', 'NbrActionSweeper', 'NbrActionSweeper/90', 'AvgLenFromGoalLine', 'Matches'
     ]
     
     data_to_csv(folder, leagues, columns,"GK_Advanced")
