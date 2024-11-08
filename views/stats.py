0208e560fed1b27a3e4c2deb79e8ba6f8891b160
noninho18 <elion.hashani@telecomnancy.net>
2024-11-08 20:18:37 +0100

    [+] Adding player stats and comparison

--- views/stats.py
+++ views/stats.py
@@ -3,11 +3,8 @@ import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import cm
 
 st.title("Player stats and comparison")
-
-filepath = "./Data/"
 
 dicoStats = {
     'Overall': "_Basic_stats.csv",
@@ -28,21 +25,45 @@ player_options = [
     for _, row in player_csv.iterrows()
 ]
 
-selected_player = st.selectbox("Select a player", player_options, index=None, placeholder="Choose a player")
-if selected_player:
-    if selected_player.split(" - ")[1] == "GK":
-        stats_options = ['Overall', 'Goalkeeper', 'Advanced Goalkeeper']
+first_player_position = player_options[0].split(" - ")[1]
+stats_options = (
+    ['Overall', 'Goalkeeper', 'Advanced Goalkeeper', 'Defensive', 'Creativity', 'Passing', 'PassTypes', 'Shooting', 'Possession', 'PlayingTime']
+)
+selected_stats = st.selectbox("Select the type of stat for all players", stats_options)
+
+def get_player_stats(filepath, player_name):
+    stats = pd.read_csv(filepath)
+    player_stats = stats[stats['Player'] == player_name]
+    if not player_stats.empty:
+        if 'Comp' in player_stats.columns:
+            stat_columns = [col for col in player_stats.columns if col not in ['Player', 'Nation', 'Pos', 'Squad', 'Comp', 'Age', 'Born']]
+        else:
+            stat_columns = [col for col in player_stats.columns if col not in ['Player', 'Nation', 'Pos', 'Squad', 'Age', 'Born']]
+        
+        percentiles = stats[stat_columns].apply(calculate_rank)
+        player_percentiles = percentiles[stats['Player'] == player_name].iloc[0] if not player_stats.empty else []
+        return player_percentiles, stat_columns
     else:
-        stats_options = ['Overall', 'Defensive', 'Creativity', 'Passing', 'PassTypes', 'Shooting', 'Possession', 'PlayingTime']
-    selected_stats = st.selectbox("Select the type of stat", stats_options)
+        return None, None
 
+def get_player_data(selected_player, selected_stats, player_idx):
+    if (selected_stats == 'Goalkeeper' or selected_stats == 'Advanced Goalkeeper') and selected_player.split(" - ")[1] != 'GK':
+        st.write(f"Selected stat type '{selected_stats}' is only available for goalkeepers.")
+        return None, None, None
+    filepath = "./Data/"
     player_name = selected_player.split(" - ")[0]
-    player_info = player_csv[(player_csv['Player'] == player_name) & (player_csv['Born'] == int(selected_player.split(" - ")[2]))]
-    player_info['Seasons'] = player_info['Seasons'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+    player_born = int(selected_player.split(" - ")[2])
 
-    player_seasons = player_info.iloc[0]['Seasons'] if not player_info.empty else {}
+    player_info = player_csv[(player_csv['Player'] == player_name) & (player_csv['Born'] == player_born)]
+    player_info_seasons = player_info['Seasons'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+    player_seasons = player_info_seasons.iloc[0] if not player_info.empty else {}
 
-    season_select = st.selectbox("Select a season or a competition", list(player_seasons.keys()) if player_seasons else [])
+    season_select = st.selectbox(
+        "Select a season",
+        list(player_seasons.keys()),
+        key=f"season_{selected_player}_{selected_stats}_{player_idx}"
+    )
+
     if season_select == "CopaAmerica2024" or season_select == "EURO 2024" or season_select == "WC 2022":
         if season_select == "CopaAmerica2024":
             filepath += "Int_comps/CopaAmerica2024/CopaAmerica2024"
@@ -57,41 +78,64 @@ if selected_player:
             filepath += season_select + "/" + dicoStats[selected_stats][1:-10] + "/" + "Top_5_Leagues" + dicoStats[selected_stats]
         else:
             filepath += season_select + "/" + dicoStats[selected_stats][1:-10] + "/" + competition_select.replace(" ", "_") + dicoStats[selected_stats]
-    stats = pd.read_csv(filepath)
-    player_stats = stats[stats['Player'] == player_name]
+    
+    try:
+        stats = pd.read_csv(filepath)
+        player_stats = stats[stats['Player'] == player_name]
+        stat_columns = [col for col in player_stats.columns if col not in ['Player', 'Nation', 'Pos', 'Squad', 'Comp', 'Age', 'Born', 'Club']]
+        
+        def calculate_rank(series):
+            series_numeric = pd.to_numeric(series, errors='coerce')
+            zero_like_mask = series_numeric.fillna(0).abs() < 0.01
+            rank_series = series_numeric.rank(method="min", ascending=False)
+            rank_series[zero_like_mask] = rank_series.max() + 1
+            max_rank = rank_series.max()
+            return (max_rank - rank_series) / max_rank * 100
+        
+        percentiles = stats[stat_columns].apply(calculate_rank)
+        player_percentiles = percentiles[stats['Player'] == player_name].iloc[0] if not player_stats.empty else []
+        return player_percentiles, stat_columns, player_name
+    except FileNotFoundError:
+        st.write("File not found. Please check the path.")
+        return None, None, None
 
-    def calculate_dense_percentile(series):
-        dense_rank = series.rank(method="dense", ascending=False)
-        max_rank = dense_rank.max()
-        return (max_rank - dense_rank) / max_rank * 100 
+columns = st.columns(3)
 
-    if 'Comp' in player_stats.columns:
-        stat_columns = [col for col in player_stats.columns if col not in ['Player', 'Nation', 'Pos', 'Squad', 'Comp', 'Age', 'Born']]
+players_data = []
+colors = ['blue', 'orange', 'green']
+
+for i in range(3):
+    with columns[i]:
+        selected_player = st.selectbox(f"Select player {i + 1}", player_options, index=None, placeholder="Choose a player", key=f"player_select_{i}")
+        
+        if selected_player:
+            player_percentiles, stat_columns, player_name = get_player_data(selected_player, selected_stats, i)
+            
+            if player_percentiles is not None:
+                players_data.append((player_percentiles, stat_columns, player_name, colors[i]))
+
+if len(players_data) > 0:
+    if stat_columns is None:
+        st.write("No stats available for the selected players.")
+        st.stop()
     else:
-        stat_columns = [col for col in player_stats.columns if col not in ['Player', 'Nation', 'Pos', 'Squad', 'Age', 'Born']]
-    percentiles = stats[stat_columns].apply(calculate_dense_percentile)
-
-    player_percentiles = percentiles[stats['Player'] == player_name].iloc[0] if not player_stats.empty else []
-
-    if not player_stats.empty:
         num_vars = len(stat_columns)
         angles = np.linspace(0, 2 * np.pi, num_vars, endpoint=False).tolist()
+        angles += angles[:1]
 
         fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
         ax.set_theta_offset(np.pi / 2)
         ax.set_theta_direction(-1)
 
-        ax.plot(angles, player_percentiles, color='blue', linewidth=2, label=player_name)
-        ax.fill(angles, player_percentiles, color='blue', alpha=0.25)
+        for percentiles, columns, name, color in players_data:
+            player_values = percentiles.tolist() + percentiles.tolist()[:1]
+            ax.plot(angles, player_values, color=color, linewidth=2, label=name)
+            ax.fill(angles, player_values, color=color, alpha=0.25)
 
         ax.set_yticks([20, 40, 60, 80, 100])
         ax.set_yticklabels(['20', '40', '60', '80', '100'])
-        ax.set_xticks(angles)
+        ax.set_xticks(angles[:-1])
         ax.set_xticklabels(stat_columns, fontsize=10)
 
+        plt.legend(loc="upper right", bbox_to_anchor=(1.3, 1.1))
         st.pyplot(fig)
-    else:
-        st.write(f"No stats available for {player_name} in {season_select}.")
-
-    st.write(f"Player stats for {player_name} in {season_select}")
-    st.write(player_stats)
